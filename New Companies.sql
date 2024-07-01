c56868f3156d041569b4183908ddadf3a8a52189
Joseph Ferguson <99895281+JosephAFerguson@users.noreply.github.com>
2024-07-01 16:24:27 -0400

    Delete New Companies.sql

--- New Companies.sql
+++
@@ -1,4 +0,0 @@
-SELECT Cm.company_code, Cm.founder,   COUNT(DISTINCT Em.lead_manager_code) , COUNT( DISTINCT Em.senior_manager_code), COUNT(DISTINCT Em.manager_code),COUNT(DISTINCT Em.employee_code)
-FROM Company Cm
-JOIN Employee Em ON Cm.company_code = Em.company_code
-GROUP BY Cm.company_code, Cm.founder;
